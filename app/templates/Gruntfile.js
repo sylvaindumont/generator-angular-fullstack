c822e97811a7db4484a50b214c591c0a94078c5e
Sylvain Dumont <sylvain.dumont35@gmail.com>
2015-03-03 16:31:40 +0100

    sass to libsass

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -574,7 +574,7 @@ module.exports = function (grunt) {
     sass: {
       server: {
         options: {
-          loadPath: [
+          includePaths: [
             '<%%= yeoman.client %>/bower_components',
             '<%%= yeoman.client %>/app',
             '<%%= yeoman.client %>/components'
